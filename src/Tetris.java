46355a2b5e4b0a229c6f9845d0b1915801d3dd89
SilentFire494 <SilentFire494@gmail.com>
2023-06-09 14:12:11 -0500

    Base for Tetris game

---
+++ src/Tetris.java
@@ -0,0 +1,30 @@
+import framework.Game;
+import framework.SceneController;
+import game.TitleScene;
+
+public class Tetris extends Game
+{
+    public Tetris()
+    {
+        super("Tetris");
+    }
+
+    public static void main(String[] args)
+    {
+        Tetris game = new Tetris();
+        Game.launchGame(game);
+    }
+
+    @Override protected void onGameStart() 
+    {
+        TitleScene titleScreen = new TitleScene("Title");
+
+        SceneController controller = this.getSceneController();
+        controller.addScene(titleScreen);
+    }
+    
+    @Override protected void onShutDown() 
+    {
+        // TODO Auto-generated method stub
+    }
+}
---
+++ src/game/TitleScene.java
@@ -0,0 +1,64 @@
+package game;
+
+import java.awt.Graphics2D;
+
+import framework.Keyboard;
+import framework.Scene;
+
+public class TitleScene extends Scene
+{
+
+    public TitleScene(String name) 
+    {
+        super(name);
+    }
+
+    @Override
+    public void load() 
+    {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void unload() 
+    {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void enter() 
+    {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void exit() 
+    {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void processInputs(double secsPerFrame, Keyboard keys) 
+    {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void update(double secsPerFrame) 
+    {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void render(Graphics2D g) 
+    {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onShutDown() 
+    {
+        // TODO Auto-generated method stub
+    }
+        
+}
